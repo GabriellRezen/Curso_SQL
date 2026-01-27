38bf5dedde74751e3fb03b588d1c96d612422089
Gabriel Rezende <20240343@iade.pt>
2026-01-27 16:11:11 +0000

    review datetime function

---
+++ curso-sql/05_select_col.sql
@@ -0,0 +1,15 @@
+SELECT  IdCliente,
+
+        QtdePontos,
+
+        QtdePontos + 10 AS QtdePontosPlus10,
+
+        QtdePontos * 2 AS QtdePontosDouble,
+
+        DtCriacao,
+
+        datetime(substr(DtCriacao, 1, 19)) AS DtCriacaoNova,
+
+        strftime('%w', datetime(substr(DtCriacao, 1, 19))) AS diaSemana 
+
+FROM CLIENTES
