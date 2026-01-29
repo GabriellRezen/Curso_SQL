09b3ad1dfc9391c170d517d24d567d1b19eb3e84
Gabriel Rezende <20240343@iade.pt>
2026-01-29 15:18:51 +0000

    exercises for where

---
+++ curso-sql/exercicios/02_where.sql
@@ -0,0 +1,9 @@
+-- Lista de pedidos realizados no fim de semana
+
+SELECT  IdTransacao,
+        DtCriacao,
+        strftime('%w', datetime(substr(DtCriacao,1,19))) AS DtCriacaoNova
+
+FROM transacoes
+
+WHERE DtCriacaoNova IN ('6','0')
